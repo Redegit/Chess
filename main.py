aaf558cf30942be308734a97a0f86b66a81e605a
Strawlll <ppkalinin@mail.ru>
2022-02-27 21:18:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
-import pygame
+import pygame as pg
+
 import sys
 
-pygame.init()
+from test import Figure
+pg.font.init()
 
-screen = pygame.display.set_mode((800, 800))
-r = pygame.Rect(50, 50, 100, 200)
-pygame.draw.rect(screen, (0, 150, 150), r, 0)
+pg.display.set_caption("CHESS")
+clock = pg.time.Clock()
+
+background = pg.image.load('фон.jpg')
+
+font = pg.font.Font('CASEFONT.TTF', 72)
+text1 = font.render('p', True, (71, 51, 51))
+sc = pg.display.set_mode((800, 700))
+sc.blit(background,(0, 0))
+image = pg.image.load('main.jpg')
+image = pg.transform.scale(image, (606, 600))
+sc.blit(image, (100, 50))
+sc.blit(text1, (135, 550))
+pg.display.update()
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    for i in pg.event.get():
+        if i.type == pg.QUIT:
             sys.exit()
-    pygame.display.flip()
-
-
-
-
-
-
-
-
+        if i.type == pg.MOUSEBUTTONDOWN:
+            if 137 < pg.mouse.get_pos()[0] < 681 and 70 < pg.mouse.get_pos()[1] < 612:
+                print(pg.mouse.get_pos()[0]//69-2, pg.mouse.get_pos()[1]//70-1)
+            print(pg.mouse.get_pos())
+    clock.tick(60)
